757a1a5c4743fa1583f28c02d0a417853296fb47
Himanshu Dadheech <68987615+himanshudadheech@users.noreply.github.com>
2021-11-09 22:48:46 +0530

    Create Add Two Number.cpp

---
+++ cpp/easy/Add Two Number.cpp
@@ -0,0 +1,17 @@
+// FLOW001
+
+#include <iostream>
+using namespace std;
+
+int c,a,b;
+int main() {
+	// your code goes here
+	 cin>>c;
+	for(int i=0;i<c;i++){
+	   
+	    cin>>a>>b;
+	    cout<<a+b<<endl;
+	
+	}
+	
+}
