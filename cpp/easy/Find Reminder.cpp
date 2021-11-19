da4b02b1f0fbf5141e8312bd65739a3eeaef0c65
Himanshu Dadheech <68987615+himanshudadheech@users.noreply.github.com>
2021-11-19 18:30:54 +0530

    Create Find Reminder.cpp

---
+++ cpp/easy/Find Reminder.cpp
@@ -0,0 +1,19 @@
+//Problem code FLOW002
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b,t,r; //7
+  cin>>t;
+
+  
+  
+  for(int i =0; i<t;i++){
+      cin>>a;
+      cin>>b;
+      int r = a%b;
+      cout<<r;
+  }
+  
+}
