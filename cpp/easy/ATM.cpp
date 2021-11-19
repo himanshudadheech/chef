fb66b74726aa39ab8b2cb0c55b7a1f229e381e75
Himanshu Dadheech <68987615+himanshudadheech@users.noreply.github.com>
2021-11-19 17:43:34 +0530

    Create ATM.cpp

---
+++ cpp/easy/ATM.cpp
@@ -0,0 +1,33 @@
+//Problem Code : HS08TEST
+
+#include <iostream>
+
+#include <iomanip>
+using namespace std;
+
+int main(){
+    //demand and bank balance
+    int d;
+    float bal;
+    
+    cout<<"Enter demand";
+    cin>>d;
+    cout<<endl;
+    
+    cout<<"Enter Balance";
+    cin>>bal;
+    
+    int g = d%5; //multiple of 5 ammount or demmand
+    float tax;
+    tax = d+0.5;
+    
+    if(g==0 && tax<bal){
+        
+        bal = bal-tax;
+        cout<<fixed<<setprecision(2)<<bal;
+    }
+    else
+    {
+        cout<<fixed<<setprecision(2)<<bal;
+    }
+}
